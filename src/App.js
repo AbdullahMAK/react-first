ed7e4a011396ed6e8cf41db17f195d8405530de3
AbdullahMAK <47482822+AbdullahMAK@users.noreply.github.com>
2021-08-25 06:32:17 +0300

    Gitting Git

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "./components/Header";
 
 function App() {
-  const name = 'Abdullah'
+
   return (
     <div className="container">
       <h1>Hello from react</h1>
