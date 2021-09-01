41a444c5c2523605dd34b3abf8e1531e686cbf5c
AbdullahMAK <47482822+AbdullahMAK@users.noreply.github.com>
2021-09-01 10:27:08 +0300

    Last commit in project and done with router and footer and  fix some designs

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom'
+
+const Footer = () => {
+    return (
+        <footer>
+            <Link to='/about'>
+                <p>Copyright &copy; 2021</p>
+            </Link>   
+             
+        </footer>
+    )
+}
+
+export default Footer
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,16 @@
 import Button from "./Button"
+import { useLocation } from "react-router-dom"
 
 const Header = ({onAdd, showAdd}) => {
+    const location = useLocation()
     
     return (
         <header className='header'>
-            <h1>To Do List</h1>
-            <Button  color={showAdd ? '#71031c' : '#04637a'} text={showAdd ? 'Close' : 'Add'} onClick={onAdd}/>
             
+            <h1>To Do List</h1>
+            {location.pathname === '/' && (
+            <Button  color={showAdd ? '#a61637' : '#08758f'} text={showAdd ? 'Close' : 'Add'} onClick={onAdd}/>
+            )}
         </header>
     )
 }
